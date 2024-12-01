f8571398dd10910374bccf4498a29523e38d33c3
Bruno <bruno@Rafaelas-MacBook-Air.local>
2024-12-01 03:12:31 -0300

    Please. GOgit add .

--- iplookup/settings.py
+++ iplookup/settings.py
@@ -95,9 +95,11 @@ WSGI_APPLICATION = 'iplookup.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default='postgres://postgres:14192ZeRuela@localhost:5432/lukeup'
+    )
 }
-
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
