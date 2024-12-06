20167f75dc82f7af1f6e49f7d2adf9fe84ebed43
Bruno <bruno@Rafaelas-MacBook-Air.local>
2024-12-05 22:30:02 -0300

    aksdfãlsdfãlkfd~aklsdf

--- iplookup/settings.py
+++ iplookup/settings.py
@@ -17,8 +17,8 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#STATIC_URL = '/static/'
-#STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
